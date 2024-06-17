566252cc06cdb7036d1f3acb3aad8f62c7993422
Christian <emicarmo@hotmail.com>
2024-06-17 17:36:07 -0300

    Usuario

---
+++ app/model/Usuarios.js
@@ -0,0 +1,5 @@
+/* class Usuarios{
+    constructor(id_usuario, usuario, email, password, nombre, apellido, fecha_nacimiento, telefono, direccion, ciudad, provincia, pais, codigo_postal, rol, status) {
+        
+    }
+} */
