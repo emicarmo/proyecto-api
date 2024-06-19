735064aaa9d6acc177d682d0040e4c1fe62b5408
Christian <emicarmo@hotmail.com>
2024-06-19 16:59:24 -0300

    agregando modelos

--- app/model/Usuarios.js
+++ app/model/Usuarios.js
@@ -1,5 +1,22 @@
-/* class Usuarios{
+class Usuarios{
     constructor(id_usuario, usuario, email, password, nombre, apellido, fecha_nacimiento, telefono, direccion, ciudad, provincia, pais, codigo_postal, rol, status) {
-        
+        this.id_usuario = id_usuario,
+        this.usuario = usuario,
+        this.email = email,
+        this.password = password,
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.fecha_nacimiento = fecha_nacimiento,
+        this.telefono = telefono,
+        this.direccion = direccion,
+        this.ciudad = ciudad,
+        this.provincia = provincia,
+        this.pais = pais,
+        this.codigo_postal = codigo_postal,
+        this.rol = rol,
+        this.status = status
     }
-} */
+}
+
+module.exports = Usuarios
+
