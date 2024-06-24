ea074d603887f877e4aedb63384b9095e5e17829
Christian <emicarmo@hotmail.com>
2024-06-24 14:22:09 -0300

    cambios en el app.js

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 const RestApiServer = require('./1.Presentation/RestApi/rest.server');
-// const FrontServer = require('./1.Presentation/Frontend/front.server')
-
-(async()=>{
-    await main();
-})();
-
+const FrontServer = require('./1.Presentation/Frontend/front.server')
 
 //entry point function
-async function main(){
 
+const restApiServer = new RestApiServer();
+const frontServer = new FrontServer();
 
-    const restApiServer = new RestApiServer();
-    // const frontServer = new FrontServer();
-    
-    restApiServer.start();
-    // frontServer.start();
-
-
-
-
-}
+restApiServer.start();
+frontServer.start();
