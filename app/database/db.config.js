db23159a93ee8a3fc7be744f61ca9591dc3bae2a
Christian <emicarmo@hotmail.com>
2024-07-01 19:38:34 -0300

    pequeños cambios

--- app/database/db.config.js
+++ app/database/db.config.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     database: process.env.DB_NAME
 });
 
-module.exports = connection;
+module.exports = pool;
