796542cee5d26cd431a8ea42d782cbd14e202e58
Jonas Muñiz <jonasmz.dev@gmail.com>
2024-07-02 19:25:44 -0300

    Sobreescribo los metodos del base repository ya que el nombre id generico no me sirve para esta tabla: se decidio mantener la nomenclatura id_tablename

--- 3.Persistence/DAO.MySql/DAORepositories/book.repository.js
+++ 3.Persistence/DAO.MySql/DAORepositories/book.repository.js
@@ -4,6 +4,29 @@ class BookRepository extends BaseRepository {
     constructor(){
         super('libros');
     }
+
+    async getAll(){
+        const sql = `SELECT libros.*, categoria.nombre_cat as categoria FROM ${this.tableName} LEFT JOIN categoria on categoria.id_categoria = libros.categoria_id`;
+        return await this.query(sql);
+                                    }
+
+    async findById(id){
+        const sql = `SELECT libros.*, categoria.nombre_cat as categoria FROM ${this.tableName} LEFT JOIN categoria on categoria.id_categoria = libros.categoria_id where libros.id_libros = ${id}`;
+        return await this.query(sql);
+    }
+
+    async delete(id){
+        const sql = `delete from ${this.tableName} where id_libros = ${id}`;
+        return await this.query(sql, [id]);
+    }
+
+    async update(entity, id){
+        this.extractData(entity);
+
+        const clouse = this.fields.map(field => `${field}=?`).join(', ');
+        const sql = `UPDATE ${this.tableName} SET ${clouse} WHERE id_libros = ?`;
+        return await this.query(sql, [...this.values, id]);
+    }
 }
 
 
