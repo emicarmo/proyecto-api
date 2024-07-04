fca348af9d9fad627101f412fb4de03989091ce4
Jonas Muñiz <jonasmz.dev@gmail.com>
2024-07-04 18:29:04 -0300

    Corrijo nombres de variables de entorno para mantener el standard

--- 3.Persistence/DAO.MySql/db.config.js
+++ 3.Persistence/DAO.MySql/db.config.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
