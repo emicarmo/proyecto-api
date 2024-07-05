86cbd4ccc7160c67d27bc04e51f06a278c4dc081
Christian <emicarmo@hotmail.com>
2024-07-05 11:48:16 -0300

    Pequeños cambios

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const RestApiServer = require('./1.Presentation/RestApi/rest.server');
 const FrontServer = require('./1.Presentation/Frontend/front.server');
 
-const restApiServer = new RestApiServer();
+(async()=>{
+    await main()
+})()
+
+async function main() {
+    const restApiServer = new RestApiServer();
 const frontServer = new FrontServer();
 
 restApiServer.start();
 frontServer.start();
+}
+
